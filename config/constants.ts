a5b78ce73b834a2bf3ecdd09e5bdf49d32dfd6b3
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-19 19:30:00 +0800

    feat: hamidashi

--- config/constants.ts
+++ config/constants.ts
@@ -1,10 +1,22 @@
+// 立绘, 只收集我自己喜欢的
+// If there is any infringement, please contact me for removal
 export const IMAGES = [
+    // 孤独摇滚tv
     'https://bocchi.rocks/tv/assets/img/page/character/hitori/main.png',
     'https://bocchi.rocks/tv/assets/img/page/character/ikuyo/main.png',
-    'https://bocchi.rocks/tv/assets/img/page/character/nijika/main.png',
-    'https://bocchi.rocks/tv/assets/img/page/character/ryo/main.png',
+    // 孤独摇滚剧场版
     'https://bocchi.rocks/omnibus/assets/img/page/character/hitori/image_movienew.png',
     'https://bocchi.rocks/omnibus/assets/img/page/character/ikuyo/image_movienew.png',
-    'https://bocchi.rocks/omnibus/assets/img/page/character/nijika/image_movienew.png',
-    'https://bocchi.rocks/omnibus/assets/img/page/character/ryo/image_movienew.png',
+
+    // 常轨脱离凸
+    'https://madosoft.net/totsu/wp/wp-content/themes/hamidashi/img/character/asumi_main.png',
+    'https://madosoft.net/totsu/wp/wp-content/themes/hamidashi/img/character/hiyori_main.png',
+    'https://madosoft.net/totsu/wp/wp-content/themes/hamidashi/img/character/kano_main.png',
+    'https://madosoft.net/totsu/wp/wp-content/themes/hamidashi/img/character/ameri_main.png',
+
+    // 常轨脱离
+    'https://madosoft.net/hamidashi/wp/wp-content/themes/hamidashi/img/character/asumi_pic.png',
+    'https://madosoft.net/hamidashi/wp/wp-content/themes/hamidashi/img/character/hiyori_pic.png',
+    'https://madosoft.net/hamidashi/wp/wp-content/themes/hamidashi/img/character/kano_pic.png',
+    'https://madosoft.net/hamidashi/wp/wp-content/themes/hamidashi/img/character/ameri_pic.png',
 ] as const;
