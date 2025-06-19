fb600f83e1cbf003ecd8942d751a4bd0ea1dd59e
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-19 22:13:39 +0800

    fix: image url start with

--- img/index.ts
+++ img/index.ts
@@ -9,12 +9,12 @@ export const getValidImageUrl = async (maxAttempts = 10) => {
     for (let i = 0; i < maxAttempts; i++) {
         const imageUrl = IMAGES[Math.floor(Math.random() * IMAGES.length)];
 
-        if (!imageUrl.startsWith('/')) {
+        if (imageUrl.startsWith('http')) {
             return { url: imageUrl, isLocal: false };
         }
 
         try {
-            const localPath = join(process.cwd(), imageUrl);
+            const localPath = join(".", imageUrl);
             await readFile(localPath);
             return { url: localPath, isLocal: true };
         } catch (error) {
